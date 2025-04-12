56150c4a34aa937e4d59cfe2221f15c6c1385f73
DHMorse <daniel@morsetech.net>
2025-04-12 09:59:31 -0500

    cleaned up the code

--- constants.py
+++ constants.py
@@ -1,6 +1,10 @@
-YAPATRON_CHANNEL_ID: int = 1359994252427268126
+ANIME_GIF_URL: str = "https://tenor.com/view/anime-gif-7279870884587886608"
 
 DOOM_GUY_ID: int = 1235011539648516136 
+
+DOOM_GUY_BOOK_URL: str = "https://www.wattpad.com/story/392586616-the-hollow-born"
+
+IZU_CUMMING_IN_A_GLASS_FILE_NAME: str = "Doro_milk_Vo5"
 
 LANGUAGE_ROLE_NAMES: list[str] = [
     "Fluent Japanese",
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import random
 import dotenv 
 import discord
 from discord.ext import commands
@@ -8,8 +7,9 @@ from typing import Optional
 from constants import (
     LANGUAGE_ROLE_NAMES,
     LIST_OF_ANIME,
-    YAPATRON_CHANNEL_ID,
-    DOOM_GUY_ID
+    DOOM_GUY_ID,
+    DOOM_GUY_BOOK_URL,
+    ANIME_GIF_URL
 )
 
 dotenv.load_dotenv()
@@ -19,7 +19,7 @@ botToken: Optional[str] = os.getenv("BOT_TOKEN")
 if not botToken:
     raise ValueError("BOT_TOKEN is not set in the environment variables")
 
-bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
+bot: commands.Bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
@@ -31,16 +31,25 @@ async def on_member_update(before: discord.Member, after: discord.Member):
     if before.roles != after.roles:
         # if the user has 3 or more roles from the LANGUAGE_ROLE_NAMES list, add the "Polyglot" role
         if len([role for role in after.roles if role.name in LANGUAGE_ROLE_NAMES]) >= 3:
-            await after.add_roles(discord.utils.get(after.guild.roles, name="Polyglot"))
+            polyglotRole = discord.utils.get(after.guild.roles, name="Polyglot")
+            if polyglotRole:
+                await after.add_roles(polyglotRole)
         else:
-            await after.remove_roles(discord.utils.get(after.guild.roles, name="Polyglot"))
+            polyglotRole = discord.utils.get(after.guild.roles, name="Polyglot")
+            if polyglotRole:
+                await after.remove_roles(polyglotRole)
+
+    if before.timeout != after.timeout:
+        if after.timeout is True:
+            channel: discord.TextChannel = discord.utils.get(after.guild.text_channels, name="yapatron")
+            await channel.send(f"{after.mention} has been deported")
 
 @bot.event
 async def on_message(message: discord.Message):
     # Check if any anime term is in the message and only send one response
     if not message.author.bot:
         if any(anime in message.content.lower() for anime in LIST_OF_ANIME):
-            await message.channel.send("https://tenor.com/view/anime-gif-7279870884587886608")
+            await message.reply(f'{ANIME_GIF_URL}')
             return
 
     if 'gay' in message.content.lower():
@@ -52,21 +61,10 @@ async def on_message(message: discord.Message):
     if message.mentions:
         for mention in message.mentions:
             if mention.id == DOOM_GUY_ID:
-                await message.reply("Check out this super family friendly [book](https://www.wattpad.com/story/392586616-the-hollow-born)")
+                await message.reply(f"Check out this super family friendly [book]({DOOM_GUY_BOOK_URL})")
 
     await bot.process_commands(message)
 
-# Send custom message on timeout
-@bot.event
-async def on_member_update(before, after) -> None:
-    if before.timeout != after.timeout:
-        if after.timeout:
-            channel: discord.TextChannel = bot.get_channel(YAPATRON_CHANNEL_ID)
-            await channel.send(f"{after.mention} has been deported")
-
-@bot.command(name="hello")
-async def hello(ctx):
-    await ctx.send("Hello!")
 
 if __name__ == "__main__":
     bot.run(botToken)
