333f767ca4af4fe596b64c96e24c4ab9f5b74008
DHMorse <daniel@morsetech.net>
2025-04-12 15:25:03 -0500

    reverted

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def on_member_update(before: discord.Member, after: discord.Member):
                 await after.remove_roles(polyglotRole)
 
     if before.timeout != after.timeout:
-        if after.timeout and not before.timeout:
+        if after.timeout:
             channel = discord.utils.get(after.guild.text_channels, name="yapatron")
             if channel:
                 await channel.send(f"{after.mention} has been deported")
