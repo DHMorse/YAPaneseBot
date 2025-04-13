80262095147855f464051aeed5e18b2df7740989
DHMorse <daniel@morsetech.net>
2025-04-12 19:47:43 -0500

    10 out of 100

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ async def on_message(message: discord.Message):
     if message.mentions:
         for mention in message.mentions:
             if mention.id == DOOM_GUY_ID:
-                if random.random() < 0.7:
+                if random.random() < 0.9:
                     await message.reply(f"Check out this super family friendly [book]({DOOM_GUY_BOOK_URL})")
 
     await bot.process_commands(message)
